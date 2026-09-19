7da1b1b4da428186b525316f6c9dfa078e6aef8e
alfaaxiter-rgb <alfaaxiter@gmail.com>
2026-09-19 20:03:12 +0700

    Update ModernDashboard.kt

--- ZalithLauncher/src/main/java/com/movtery/zalithlauncher/ui/screens/content/ModernDashboard.kt
+++ ZalithLauncher/src/main/java/com/movtery/zalithlauncher/ui/screens/content/ModernDashboard.kt
@@ -29,6 +29,7 @@ import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
