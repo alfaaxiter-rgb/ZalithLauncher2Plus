bb48061aae6def32ff866c6fcc3c6bc6d990a583
alfaaxiter-rgb <alfaaxiter@gmail.com>
2026-09-26 16:26:58 +0700

    Fix dashboard Text import after icon migration

--- ZalithLauncher/src/main/java/com/movtery/zalithlauncher/ui/screens/content/ModernDashboard.kt
+++ ZalithLauncher/src/main/java/com/movtery/zalithlauncher/ui/screens/content/ModernDashboard.kt
@@ -22,6 +22,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
