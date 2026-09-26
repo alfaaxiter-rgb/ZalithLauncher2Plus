d5a546cad79f82a039a89b000c47544c1b292df0
alfaaxiter-rgb <alfaaxiter@gmail.com>
2026-09-26 22:23:05 +0700

    Fix recordings neon control imports

--- ZalithLauncher/src/main/java/com/movtery/zalithlauncher/ui/screens/content/RecordingsScreen.kt
+++ ZalithLauncher/src/main/java/com/movtery/zalithlauncher/ui/screens/content/RecordingsScreen.kt
@@ -29,6 +29,8 @@ import android.provider.MediaStore
 import android.text.format.Formatter
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.Image
@@ -66,6 +68,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
