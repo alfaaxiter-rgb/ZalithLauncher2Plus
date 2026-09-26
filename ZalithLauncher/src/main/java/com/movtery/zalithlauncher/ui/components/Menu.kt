5552e430e4d0e1e176dcfefeac6048d07303d7a8
alfaaxiter-rgb <alfaaxiter@gmail.com>
2026-09-26 15:38:00 +0700

    Fix remaining Menu neon slider imports

--- ZalithLauncher/src/main/java/com/movtery/zalithlauncher/ui/components/Menu.kt
+++ ZalithLauncher/src/main/java/com/movtery/zalithlauncher/ui/components/Menu.kt
@@ -38,11 +38,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
